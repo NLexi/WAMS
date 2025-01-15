bf33b884e680c8769da24ed1138fe17b9fd51cde
NLexi <lexi.nicholas03@gmail.com>
2025-01-15 16:01:07 +0700

    deleted temporary form page

--- app/formtrial/page.tsx
+++
@@ -1,9 +0,0 @@
-import { POForm } from "@/components/custom/POForm";
-
-export default function FormTrial() {
-    return (
-        <div className="container m-auto p-4">
-            <POForm />
-        </div>
-    )
-}
